be5fc5f63d35ce645fb8ac984c7340f108ab77b4
daw-dev <dawdev04@gmail.com>
2023-07-04 10:47:13 +0200

    gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/daw-dev.github.io/'
+  plugins: [react()]
 })
