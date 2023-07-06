8cc34931496a1a4718c16009c6d37cb857682522
daw-dev <dawdev04@gmail.com>
2023-07-06 13:30:09 +0200

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
 import ThemeChooser from "./components/ThemeChooser";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <>
       <h1>daw._.dev</h1>
